19079da228070f9b0d7a7d57a2276b6e7c34280c
Ngan Le <kagonu@gmail.com>
2020-05-20 10:04:09 +0700

    initial commit

---
+++ Session03/menu.py
@@ -0,0 +1,16 @@
+# monan1 = 'phở'
+# monan2 = 'bún chả'
+# monan3 = 'trứng rán'
+# monan4 = 'thịt chó'
+# monan5 = 'cơm'
+
+# monan = ['phở', 'bún chả', 'trứng rán', 'thịt chó']
+# for i in range(len(monan)):
+#     print(i+1, monan[i])
+
+# if update_value in monan:
+#     index = monan.index(update_value)
+# update_value = input('nhập tên món ăn muốn đổi: ')
+
+# monan[index] = inp(ut('nhập tên món ăn mới: ')
+# print(monan)
---
+++ Session03/word_jumble.py
@@ -0,0 +1,15 @@
+from random import shuffle
+from getpass import getpass
+
+quiz = getpass('Enter your quiz: ').lower()
+
+list_quiz = list(quiz)
+shuffle(list_quiz)
+for i in range(len(list_quiz)):
+    print(list_quiz[i].upper(), end=' ')
+
+ans = input('Your guess? ')
+if ans == quiz:
+    print('Bingo!!')
+else:
+    print(':(')
